0382149a9c27654eee62569715ac2ee7f2663977
Anni Kynsilehto <anni.kynsilehto@gmail.com>
2021-07-08 17:19:15 +0300

    Create simple table of contents and show selected rules in a separate div

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
+
 import { parseRules } from "../utils/ruleParser";
 
 export default function Home(props) {
+  const rules = JSON.parse(props.rules);
+
+  const [selectedRules, setSelectedRules] = useState();
+  const selectRules = (rules) => setSelectedRules(rules);
+
   return (
     <div>
       <Head>
@@ -15,7 +23,30 @@ export default function Home(props) {
 
       <main>
         <h1>Magic: The Gathering Comprehensive Rules</h1>
-        <div>{props.rules}</div>
+        <div>
+          <p>Table of Contents</p>
+          <ul>
+            {rules.map((section, i) => (
+              <li key={i}>
+                {section.name}
+                <ul>
+                  {section.chapters.map((chapter, i) => (
+                    <li key={i}>
+                      <Link href="#" scroll={false}>
+                        <button onClick={() => selectRules(chapter.rules)}>
+                          {chapter.name}
+                        </button>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <div>{selectedRules}</div>
+        </div>
       </main>
     </div>
   );
