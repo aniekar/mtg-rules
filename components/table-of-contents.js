d88b2a9eb968c3f8647ab8e02dab3a3d59d4a75f
Anni Kynsilehto <anni.kynsilehto@gmail.com>
2021-07-18 14:21:24 +0300

    Make sections foldable to improve navigation

--- components/table-of-contents.js
+++ components/table-of-contents.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { forwardRef } from 'react';
+import { forwardRef, useState, useImperativeHandle } from 'react';
 
 const ChapterLink = forwardRef(({ onClick, href, chapter }, ref) => {
   return (
@@ -11,24 +11,50 @@ const ChapterLink = forwardRef(({ onClick, href, chapter }, ref) => {
 
 ChapterLink.displayName = 'ChapterLink';
 
+const FoldableSection = forwardRef(({ section }, ref) => {
+  const [visible, setVisible] = useState(false);
+
+  const showWhenVisible = { display: visible ? '' : 'none' };
+  const chevronDirection = visible ? 'chevron top' : 'chevron bottom';
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility,
+    };
+  });
+
+  return (
+    <li className="section">
+      {section.name}
+      <button onClick={toggleVisibility} className="foldButton">
+        <span className={chevronDirection} />
+      </button>
+      <ul className="chapterList" style={showWhenVisible}>
+        {section.chapters.map((chapter, i) => (
+          <li key={i} className="chapter">
+            <Link href={`/chapters/${chapter.number}`} passHref>
+              <ChapterLink chapter={chapter} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+});
+
+FoldableSection.displayName = 'FoldableSection';
+
 export default function TableOfContents({ contents }) {
   return (
     <div className="tableOfContents">
       <h2>Table of Contents</h2>
       <ul>
         {contents.map((section, i) => (
-          <li key={i} className="section">
-            {section.name}
-            <ul className="chapterList">
-              {section.chapters.map((chapter, i) => (
-                <li key={i} className="chapter">
-                  <Link href={`/chapters/${chapter.number}`} passHref>
-                    <ChapterLink chapter={chapter} />
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </li>
+          <FoldableSection key={i} section={section} />
         ))}
       </ul>
     </div>
